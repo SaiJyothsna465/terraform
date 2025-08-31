62781ecc6eddc999b03495db3c761b2fd6a84fed
saijyothsna <saijyothsnapothireddy@gmail.com>
2025-08-31 19:31:34 +0530

    count based loops done

---
+++ loops/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "roboshop"{
+    count = 4
+    ami = var.ami_id  # left and right side no need to be sync. dont change left side variable name(ami)
+    instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = {
+        Name = var.instances[count.index]
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+    name = var.sg_name
+    description = var.sg_desc
+
+    ingress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = var.protocol
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = var.ipv6_cidr_blocks
+    }
+
+    egress {
+        from_port        = var.from_port
+        to_port          = var.to_port
+        protocol         = var.protocol
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = var.ipv6_cidr_blocks
+    }
+
+    tags = var.sg_tags
+}
---
+++ variables/variables.tf
@@ -0,0 +1,55 @@
+variable "ami_id"{
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "AMI ID of joindevops RHEL9"
+}
+
+variable "instance_type"{
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+  default = {
+    Name = "HelloWorld"
+    Purpose = "variable_demo"
+  }
+}
+
+variable "sg_name"{
+    default = "allow_all_change"
+}
+
+variable "sg_desc" {
+  default = "allow all traffic"
+}
+
+variable "from_port"{
+    type = number
+    default=0
+}
+
+variable "to_port"{
+    type = number
+    default=0
+}
+
+variable "protocol"{
+    default="-1"
+}
+
+variable "cidr_blocks"{
+    type = list (string)
+    default=["0.0.0.0/0"]
+}
+
+variable "ipv6_cidr_blocks" {
+    type = list(string)
+    default = [ "::/0" ]
+}
+
+variable "sg_tags" {
+  type=map(string)
+  default = {
+    Name = "allow-all"
+  }
+}
